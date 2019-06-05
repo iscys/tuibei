07a556f1dcead3c41c5da9fde09051fe422db28c
Administrator <asa>
2019-06-05 11:46:10 +0800

    tui

--- src/main/java/com/tuibei/controller/kuaidi/ExpressController.java
+++ src/main/java/com/tuibei/controller/kuaidi/ExpressController.java
@@ -2,8 +2,11 @@ package com.tuibei.controller.kuaidi;
 
 
 import com.tuibei.model.Constant;
+import com.tuibei.model.KuaidiDisNiaoModel;
 import com.tuibei.model.TrackInfo;
+import com.tuibei.service.kuaidi.ExpressService;
 import com.tuibei.utils.ResultObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/exp")
 public class ExpressController {
 
+    @Autowired
+    private ExpressService express;
 
     /**
      * 快递单号查询
@@ -25,7 +30,7 @@ public class ExpressController {
         if(StringUtils.isEmpty(trackNum)){
             return ResultObject.build(Constant.TRACK_NUM_NULL,null,Constant.TRACK_NUM_NULL_MESSAGE);
         }
-
+        KuaidiDisNiaoModel scanInfo= express.orderScan(trackInfo);
         return null;
     }
 }
--- src/main/java/com/tuibei/service/kuaidi/ExpressService.java
+++ src/main/java/com/tuibei/service/kuaidi/ExpressService.java
@@ -1,4 +1,13 @@
 package com.tuibei.service.kuaidi;
 
+import com.tuibei.model.KuaidiDisNiaoModel;
+import com.tuibei.model.TrackInfo;
+
 public interface ExpressService {
+    /**
+     * 订单识别信息，通过单号查询查询出单号所属的物流公司
+     * @param trackInfo
+     * @return
+     */
+    KuaidiDisNiaoModel orderScan(TrackInfo trackInfo);
 }
