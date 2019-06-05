61a8df5f8a8b3980f8034cc8893287af6a1574d6
Administrator <asa>
2019-06-05 11:25:17 +0800

    tui

---
+++ src/main/java/com/tuibei/utils/ResultObject.java
@@ -0,0 +1,74 @@
+package com.tuibei.utils;
+
+
+import java.io.Serializable;
+
+/**
+ * 返回前端的数据结构
+ * @author iscys
+ */
+public class ResultObject implements Serializable {
+
+    //**返回成功失败标志**//
+    private String flag;
+    //**返回的成功错误信息**//
+    private String error;
+    //**返回的数据**//
+    private Object data;
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    /**
+     * 自定义构造返回数据结构类型
+     * @param flag
+     * @param error
+     * @param data
+     * @return
+     */
+    public static ResultObject build (String flag,String error,Object data){
+
+        return  new ResultObject(flag,error,data);
+    }
+
+
+
+    ResultObject(String flag, String error , Object data){
+        this.flag=flag;
+        this.error=error;
+        this.data=data;
+    }
+
+    public static ResultObject success (Object data){
+
+        return  new ResultObject("0","success",data);
+    }
+
+    public static ResultObject error (Object data){
+
+        return  new ResultObject("1","error",data);
+    }
+
+
+}
