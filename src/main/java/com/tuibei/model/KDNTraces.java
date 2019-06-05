931bb32eb7bafb096909dee1cf53c6dbdeb325ea
Administrator <asa>
2019-06-05 11:20:26 +0800

    tui

--- src/main/java/com/tuibei/model/KDNTraces.java
+++ src/main/java/com/tuibei/model/KDNTraces.java
@@ -10,4 +10,20 @@ public class KDNTraces implements Serializable {
 
     private String AcceptStation;//快件描述
     private String AcceptTime;//快件时间
+
+    public String getAcceptStation() {
+        return AcceptStation;
+    }
+
+    public void setAcceptStation(String acceptStation) {
+        AcceptStation = acceptStation;
+    }
+
+    public String getAcceptTime() {
+        return AcceptTime;
+    }
+
+    public void setAcceptTime(String acceptTime) {
+        AcceptTime = acceptTime;
+    }
 }
