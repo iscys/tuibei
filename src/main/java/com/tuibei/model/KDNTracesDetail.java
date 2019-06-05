f6c9594335a5a7ea3ba8becf549f2a7499f6760f
Administrator <asa>
2019-06-05 11:19:48 +0800

    tui

--- src/main/java/com/tuibei/model/KDNTracesDetail.java
+++ src/main/java/com/tuibei/model/KDNTracesDetail.java
@@ -16,4 +16,61 @@ public class KDNTracesDetail implements Serializable {
     private String Success;
     private String ShipperCode;//快递编码
     private Integer State;//物流状态: 0-无轨迹，1-已揽收，2-在途中，3-签收,4-问题件
+
+
+    public String getLogisticCode() {
+        return LogisticCode;
+    }
+
+    public void setLogisticCode(String logisticCode) {
+        LogisticCode = logisticCode;
+    }
+
+    public List<KDNTraces> getTraces() {
+        return Traces;
+    }
+
+    public void setTraces(List<KDNTraces> traces) {
+        Traces = traces;
+    }
+
+    public String getEBusinessID() {
+        return EBusinessID;
+    }
+
+    public void setEBusinessID(String EBusinessID) {
+        this.EBusinessID = EBusinessID;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
+    }
+
+    public String getSuccess() {
+        return Success;
+    }
+
+    public void setSuccess(String success) {
+        Success = success;
+    }
+
+    public String getShipperCode() {
+        return ShipperCode;
+    }
+
+    public void setShipperCode(String shipperCode) {
+        ShipperCode = shipperCode;
+    }
+
+    public Integer getState() {
+        return State;
+    }
+
+    public void setState(Integer state) {
+        State = state;
+    }
 }
