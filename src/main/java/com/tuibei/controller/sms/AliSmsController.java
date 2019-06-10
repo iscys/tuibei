f792b5e7313b5c649623fc366e8c9748c136014b
Administrator <asa>
2019-06-10 17:41:10 +0800

    tui

--- src/main/java/com/tuibei/controller/sms/AliSmsController.java
+++ src/main/java/com/tuibei/controller/sms/AliSmsController.java
@@ -1,5 +1,6 @@
 package com.tuibei.controller.sms;
 
+import com.tuibei.model.Constant;
 import com.tuibei.model.PhoneCode;
 import com.tuibei.utils.ResultObject;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,9 @@ public class AliSmsController {
 
     @RequestMapping("/code")
     public ResultObject code(PhoneCode phoneInfo){
-
+        if(phoneInfo.getType()==null){
+            return ResultObject.build(Constant.VALIDATE_CODE_TYPE,Constant.VALIDATE_CODE_TYPE_MESSAGE,null);
+        }
 
         return null;
     }
