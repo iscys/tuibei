dd325442af00c5ba13223f7e6ae1a85f72c373f1
iscys <iscys>
2019-06-10 23:15:30 +0800

    init

--- src/main/java/com/tuibei/mapper/sms/AliSmsMapper.java
+++ src/main/java/com/tuibei/mapper/sms/AliSmsMapper.java
@@ -1,4 +1,7 @@
 package com.tuibei.mapper.sms;
 
+import com.tuibei.model.sms.PhoneCode;
+
 public interface AliSmsMapper {
+    void saveSmsCode(PhoneCode phoneInfo);
 }
--- src/main/java/com/tuibei/service/sms/impl/AliSmsServiceImpl.java
+++ src/main/java/com/tuibei/service/sms/impl/AliSmsServiceImpl.java
@@ -6,13 +6,18 @@ import com.tuibei.model.constant.Constant;
 import com.tuibei.model.sms.PhoneCode;
 import com.tuibei.model.user.User;
 import com.tuibei.service.sms.AliSmsService;
+import com.tuibei.utils.DateUtils;
 import com.tuibei.utils.ResultObject;
 import com.tuibei.utils.ToolsUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AliSmsServiceImpl implements AliSmsService {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     
     @Autowired
@@ -20,6 +25,7 @@ public class AliSmsServiceImpl implements AliSmsService {
 
     @Autowired
     private UserMapper userMapper;
+
     /**
      *
      * 发送验证码
@@ -29,17 +35,23 @@ public class AliSmsServiceImpl implements AliSmsService {
      */
     @Override
     public ResultObject toSendCode(PhoneCode phoneInfo) throws Exception {
+        String phone = phoneInfo.getPhone();
+        logger.info("用户: {} 发送验证码开始",phone);
         //type =1 是注册用户
         if(phoneInfo.getType()==1){
             User user =new User();
             user.setPhone(phoneInfo.getPhone());
             User isExist = userMapper.getUserInfo(user);
+            user =null;//help gc
             if(null!=isExist){
+                logger.warn("用户已经被注册过了：{} ",phone);
                 return ResultObject.build(Constant.MEMBER_EXIST,Constant.MEMBER_EXIST_MESSAGE,null);
             }
             int code = ToolsUtils.fourCode();
             phoneInfo.setCode(code);
-
+            phoneInfo.setStart_time(DateUtils.getTimeInSecond());
+            smsMapper.saveSmsCode(phoneInfo);
+            logger.info("用户: {} 发送验证码结束",phone);
 
         }
            
