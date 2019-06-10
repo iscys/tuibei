83f45826d3cc516dbd1faadeae332d9ad8263145
Administrator <asa>
2019-06-10 14:55:25 +0800

    tui

---
+++ src/main/java/com/tuibei/model/User.java
@@ -0,0 +1,156 @@
+package com.tuibei.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String id;
+    //性别
+    private String sex;
+    //用户唯一的 member_id
+    private String member_id;
+    //昵称
+    private String nickname;
+    //用户姓名
+    private String username;
+    //用户手机
+    private String phone;
+    //用户密码
+    private String password;
+    //openid
+    private String openid;
+    //unionid
+    private String unionid;
+    //微信code
+    private String code;
+    //用户头像
+    private String headimgurl;
+    //邀请码,每一个用户会拥有一个邀请码
+    private String invite_code;
+    //用户来源
+    private String origin;//0 为app 来源 1 为小程序来源
+    //用户等级
+    private String level_id;
+    //用户上一级member_id
+    private String master;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(String member_id) {
+        this.member_id = member_id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getUnionid() {
+        return unionid;
+    }
+
+    public void setUnionid(String unionid) {
+        this.unionid = unionid;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getHeadimgurl() {
+        return headimgurl;
+    }
+
+    public void setHeadimgurl(String headimgurl) {
+        this.headimgurl = headimgurl;
+    }
+
+    public String getInvite_code() {
+        return invite_code;
+    }
+
+    public void setInvite_code(String invite_code) {
+        this.invite_code = invite_code;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getLevel_id() {
+        return level_id;
+    }
+
+    public void setLevel_id(String level_id) {
+        this.level_id = level_id;
+    }
+
+    public String getMaster() {
+        return master;
+    }
+
+    public void setMaster(String master) {
+        this.master = master;
+    }
+}
