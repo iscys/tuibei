822d9a4a651058eb97a922614cf90e80a217c46c
Administrator <asa>
2019-06-10 17:59:33 +0800

    tui

---
+++ src/main/java/com/tuibei/utils/AliSmsUtils.java
@@ -0,0 +1,64 @@
+package com.tuibei.utils;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AliSmsUtils {
+
+    @Value("${alibaba.sms.accessKeyId}")
+    private String accessKeyId;
+    @Value("${alibaba.sms.accessKeySecret}")
+    private String accessKeySecret;
+    @Value("${alibaba.sms.templateLogin}")
+    private String templateLogin;
+    /**
+     * 发送短信验证码
+     * 具体查看阿里云验证码demo以及sdk；
+     */
+    public  String sendSms()throws Exception {
+
+        String accessKeyId = accessKeyId;
+        String accessKeySecret = accessKeySecret;
+        String phone = sms.getPhone();
+        String product = sms.product;
+        String domain = sms.domain;
+        String templateCode = sms.getTemplateCode();
+        String json = sms.getTemplateParam();
+        String signName = sms.getSignName();
+
+        //可自助调整超时时间
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+        //初始化acsClient,暂不支持region化
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+        IAcsClient acsClient = new DefaultAcsClient(profile);
+        //组装请求对象-具体描述见控制台-文档部分内容
+        SendSmsRequest request = new SendSmsRequest();
+        //必填:待发送手机号
+        request.setPhoneNumbers(phone);
+        //必填:短信签名-可在短信控制台中找到
+        request.setSignName(signName);
+        //必填:短信模板-可在短信控制台中找到//"SMS_151625215"
+        request.setTemplateCode(templateCode);
+        //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        request.setTemplateParam(json);
+
+        //选填-上行短信扩展码(无特殊需求用户请忽略此字段)
+        //request.setSmsUpExtendCode("90997");
+
+        //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+        request.setOutId("yourOutId");
+        //hint 此处可能会抛出异常，注意catch
+        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+        return sendSmsResponse.getCode();
+    }
+
+}
