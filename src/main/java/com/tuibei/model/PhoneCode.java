69c5291eb5d3bfae363d2298c28c32c99655bbc5
Administrator <asa>
2019-06-10 17:32:32 +0800

    tui

---
+++ src/main/java/com/tuibei/model/PhoneCode.java
@@ -0,0 +1,74 @@
+package com.tuibei.model;
+
+import java.io.Serializable;
+
+public class PhoneCode implements Serializable {
+
+    private String id;
+    //手机号码
+    private String phone;
+    //手机验证码
+    private String code;
+    //开始时间
+    private String start_time;
+    //过期时间
+    private String expire_time;
+    private String content;
+    private String flag;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public String getExpire_time() {
+        return expire_time;
+    }
+
+    public void setExpire_time(String expire_time) {
+        this.expire_time = expire_time;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+}
