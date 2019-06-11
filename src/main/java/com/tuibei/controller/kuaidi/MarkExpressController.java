9e9511bd1b692e2749fccdcdebe282d90bfdbed3
iscys <iscys>
2019-06-11 18:44:47 +0800

    init

--- src/main/java/com/tuibei/controller/kuaidi/MarkExpressController.java
+++ src/main/java/com/tuibei/controller/kuaidi/MarkExpressController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/record")
+@RequestMapping("/mark")
 public class MarkExpressController extends BaseController {
 
 
