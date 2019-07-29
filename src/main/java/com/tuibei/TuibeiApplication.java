57165fb64906ab48e3e721f8447a4d9935fbe6a9
iscys <iscys>
2019-07-29 23:19:41 +0800

    init

--- src/main/java/com/tuibei/TuibeiApplication.java
+++ src/main/java/com/tuibei/TuibeiApplication.java
@@ -3,14 +3,7 @@ package com.tuibei;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.Annotation;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @MapperScan("com.tuibei.mapper")
